58acc0561a16c16f76dd5a562607a544df774bb3
Bananums <sjohnsen.valle@gmail.com>
2025-12-02 21:13:00 +0100

    Solved day02 part2

--- day02/main.go
+++ day02/main.go
@@ -20,16 +20,18 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println("Part 1:", solvePart1(lines))
-	fmt.Println("Part 2:", solvePart2(lines))
+	fmt.Println("Part 1:", solvePart1(lines, false))
+	fmt.Println("Part 2:", solvePart2(lines, false))
 }
 
-func solvePart1(lines []string) int {
+func solvePart1(lines []string, verbose bool) int {
 
 	sum := 0
 
 	for _, line := range lines {
-		fmt.Println(line)
+		if verbose {
+			fmt.Println(line)
+		}
 
 		rangeSpanStr := strings.Split(line, "-")
 		var rangeSpan [2]int
@@ -56,7 +58,9 @@ func solvePart1(lines []string) int {
 			}
 
 			if invalidId {
-				fmt.Println(numberStr, "-", digits)
+				if verbose {
+					fmt.Println(numberStr, "-", digits)
+				}
 				sum += i
 			}
 
@@ -66,6 +70,63 @@ func solvePart1(lines []string) int {
 	return sum
 }
 
-func solvePart2(lines []string) int {
-	return 0
+func solvePart2(lines []string, verbose bool) int {
+
+	sum := 0
+
+	for _, line := range lines {
+		rangeSpanStr := strings.Split(line, "-")
+		var rangeSpan [2]int
+		rangeSpan[0], _ = strconv.Atoi(rangeSpanStr[0])
+		rangeSpan[1], _ = strconv.Atoi(rangeSpanStr[1])
+
+		for i := rangeSpan[0]; i <= rangeSpan[1]; i++ {
+			numberStr := strconv.Itoa(i)
+			digits := len(numberStr)
+			sum += BruteForceSearch(i, numberStr, digits, verbose)
+		}
+	}
+
+	return sum
+}
+
+func BruteForceSearch(number int, numberStr string, digits int, verbose bool) int {
+	sum := 0
+	approvedPattern := ""
+
+	//Only checking half into the number, since a sequence cannot exist with more than that
+	for i := 1; i <= digits/2; i++ {
+		if numberStr == approvedPattern {
+			break
+		}
+
+		if digits%i != 0 {
+			continue
+		}
+
+		var ok = true
+		for k := 0; k < digits; k++ {
+			if numberStr[k] != numberStr[k%i] {
+				ok = false
+				break
+			}
+		}
+
+		if !ok {
+			continue
+		}
+
+		reappearance := digits / i
+		if reappearance < 2 {
+			continue
+		}
+
+		if verbose {
+			fmt.Println(numberStr)
+		}
+		sum += number
+		approvedPattern = numberStr
+	}
+
+	return sum
 }
