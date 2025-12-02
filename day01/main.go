171806d8cc20ade1a3cbfbf643264e9727cbc649
Bananums <sjohnsen.valle@gmail.com>
2025-12-02 04:56:02 +0100

    Solved day01 part2

--- day01/main.go
+++ day01/main.go
@@ -22,14 +22,16 @@ func main() {
 		log.Fatal(err)
 	}
 
+	fmt.Println("Puzzle Largest number:", FindSetLargest(lines))
+
 	fmt.Println("Part 1:", solvePart1(lines, false))
-	fmt.Println("Part 2:", solvePart2(lines))
+	fmt.Println("Part 2:", solvePart2(lines, false))
 }
 
 func solvePart1(lines []string, verbose bool) int {
-	var dial int = 50
-	var hits int = 0
-	var rollover int = 100
+	var dial = 50
+	var hits = 0
+	var rollover = 100
 
 	for _, line := range lines {
 		firstChar := string(line[0])
@@ -37,7 +39,8 @@ func solvePart1(lines []string, verbose bool) int {
 		number, _ := strconv.Atoi(numberStr)
 
 		if number >= rollover {
-			number = number % 100 // Does not handle numbers above 1000
+			// Does not handle numbers above 999. No point in doing more, since largest puzzle number is 997
+			number = number % 100
 		}
 
 		if firstChar == "R" {
@@ -66,6 +69,64 @@ func solvePart1(lines []string, verbose bool) int {
 	return hits
 }
 
-func solvePart2(lines []string) int {
-	return 0
+func solvePart2(lines []string, verbose bool) int {
+	var dial = 50
+	var hits = 0
+	var rollover = 100
+
+	for _, line := range lines {
+		firstChar := string(line[0])
+		numberStr := line[1:]
+		number, _ := strconv.Atoi(numberStr)
+
+		for number > 100 {
+			hits++
+			number -= 100
+		}
+
+		dialOld := dial
+		if firstChar == "R" {
+			if (dial + number) >= rollover {
+				dial = (dial + number) - rollover
+				if dial != 0 && dialOld != 0 {
+					hits++
+				}
+			} else {
+				dial += number
+			}
+		} else {
+			if (dial - number) < 0 {
+				dial = rollover - (number - dial)
+				if dial != 0 && dialOld != 0 {
+					hits++
+				}
+			} else {
+				dial -= number
+			}
+		}
+
+		if dial == 0 {
+			hits++
+		}
+
+		if verbose {
+			fmt.Println(line, "- dial:", dial, "hits:", hits)
+		}
+
+	}
+
+	return hits
+
+}
+
+func FindSetLargest(lines []string) int {
+	var number = 0
+	for _, line := range lines {
+		locNumberStr := line[1:]
+		locNumber, _ := strconv.Atoi(locNumberStr)
+		if locNumber > number {
+			number = locNumber
+		}
+	}
+	return number
 }
