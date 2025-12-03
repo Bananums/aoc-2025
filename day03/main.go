d3076b00be0178c6c3bbf30ac9cb94dc40ced39b
Bananums <sjohnsen.valle@gmail.com>
2025-12-03 21:21:04 +0100

    Solved day03 part02

--- day03/main.go
+++ day03/main.go
@@ -18,11 +18,11 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println("Part 1:", solvePart1(lines))
-	fmt.Println("Part 2:", solvePart2(lines))
+	fmt.Println("Part 1:", solvePart1(lines, false))
+	fmt.Println("Part 2:", solvePart2(lines, false))
 }
 
-func solvePart1(lines []string) int {
+func solvePart1(lines []string, verbose bool) int {
 
 	joltage := 0
 
@@ -33,7 +33,7 @@ func solvePart1(lines []string) int {
 		var largestIndex = 0
 
 		var largestSecond uint8 = '0'
-		//var largestIndexSecond = 0
+		var largestIndexSecond = 0
 
 		for index := range line {
 			if line[index] > largest && index < len(line)-1 {
@@ -46,20 +46,77 @@ func solvePart1(lines []string) int {
 		for index := range sub {
 			if sub[index] > largestSecond {
 				largestSecond = sub[index]
-				//largestIndexSecond = index + 1
+				largestIndexSecond = index + 1
 			}
 		}
 
-		//fmt.Println(string(largest), "-", largestIndex, "-", string(largestSecond), "-", largestIndexSecond)
+		if verbose {
+			fmt.Println(string(largest), "-", largestIndex, "-",
+				string(largestSecond), "-", largestIndexSecond)
+		}
 
 		value := (largest-'0')*10 + (largestSecond - '0')
-		//fmt.Println("Value:", value)
+		if verbose {
+			fmt.Println("Value:", value)
+		}
 		joltage += int(value)
 	}
 
 	return joltage
 }
 
-func solvePart2(lines []string) int {
-	return 0
+func solvePart2(lines []string, verbose bool) int64 {
+
+	var joltage int64 = 0
+	const digitSize = 12
+
+	for _, line := range lines {
+		allowedRemovals := len(line) - digitSize
+
+		//Just allocating 100 int32 numbers
+		var result [100]int32
+		var resultHuman [100]int32
+
+		position := 0
+		for i, digit := range line {
+
+			//Add the first number to get started
+			if i == 0 {
+				result[0] = digit
+				resultHuman[0] = digit - '0'
+				continue
+			}
+
+			for position >= 0 && allowedRemovals > 0 && result[position] < digit {
+				position--
+				allowedRemovals--
+			}
+
+			position++
+			result[position] = digit
+			resultHuman[position] = digit - '0'
+
+		}
+
+		var movedResult [digitSize]int32
+		for i, digit := range result[:digitSize] {
+			movedResult[i] = digit
+		}
+
+		if verbose {
+			fmt.Println(line, "-->", resultHuman, "-", twelveAsciiDigitsToNumber(movedResult))
+		}
+
+		joltage += twelveAsciiDigitsToNumber(movedResult)
+	}
+
+	return joltage
+}
+
+func twelveAsciiDigitsToNumber(digits [12]int32) int64 {
+	var number int64
+	for _, digit := range digits {
+		number = number*10 + int64(digit-'0')
+	}
+	return number
 }
